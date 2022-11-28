c9041e851052072d31fa4bb90f60d9fe24e5001e
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2022-11-28 23:52:16 +0500

    string_len07

--- string_len07.py
+++ string_len07.py
@@ -8,7 +8,9 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    if len(s2)%2==1 and len(s3)%2==1:
+    if len(s1)%2==1 and len(s2)%2==1 and len(s3)%2==1:
+        return f'[{s1},{s2},{s3}]'
+    elif len(s2)%2==1 and len(s3)%2==1:
         return f'[{s2},{s3}]'
     elif len(s1)%2==1 and len(s2)%2==1:
         return f'[{s1},{s2}]'
@@ -20,4 +22,4 @@ def main(s1,s2,s3):
         return f'[{s3}]'
     else:
         return '[]'
-print(main('codess','prograw','trew'))
+print(main('codes','prograw','atrew'))
