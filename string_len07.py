c417f7a83d54f7c194ca86464e6170f3803f0b07
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2022-11-29 00:34:39 +0500

    string_len07

--- string_len07.py
+++ string_len07.py
@@ -9,17 +9,17 @@ def main(s1,s2,s3):
         string
     """
     if len(s1)%2==1 and len(s2)%2==1 and len(s3)%2==1:
-        return [s1],[s2],[s3]
-    elif len(s2)%2==1 and len(s3)%2==1:
-        return [s2],[s3]
-    elif len(s1)%2==1 and len(s2)%2==1:
-        return [s1],[s2]
-    elif len(s1)%2==1:
-        return [s1]
-    elif len(s2)%2==1:
-        return [s2]
-    elif len(s3)%2==1:
-        return [s3]
+        return f'[{s1},{s2},{s3}]'
+    if len(s2)%2==1 and len(s3)%2==1:
+        return f'[{s2},{s3}]'
+    if len(s1)%2==1 and len(s2)%2==1:
+        return f'[{s1},{s2}]'
+    if len(s1)%2==1:
+        return f'[{s1}]'
+    if len(s2)%2==1:
+        return f'[{s2}]'
+    if len(s3)%2==1:
+        return f'[{s3}]'
     else:
         return '[]'
-print(main('code','prograe','atree'))
+print(main('codess','prograw','atrew'))
